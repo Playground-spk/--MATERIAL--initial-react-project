5cd4f3f19e7be7effb1cbf4acc0b695a4c029016
supakon.c <taresupakon@gmail.com>
2021-03-18 15:32:12 +0700

    [UPDATE] add class RouteComponent

--- src/config/role.config.js
+++ src/config/role.config.js
@@ -1,25 +1,34 @@
 import Login from "../views/pages/login/Login";
 
-const components = {
-    login : {
-        component: Login,
-        path:'/login',
-        name:'login'
-    }
+/**
+ * factory Route that relavant  link, title in page and maybe permission .
+ *
+ * @param {Component} component  component that show in this particular path.
+ * @param {ENUM Varriable} groupPath specific group that lead on endpoint in prop path use to grouping endpoint that related each other.
+ * @param {String} endpoint The string following groupPath in prop path that tell you about and direct to specific page .
+ * @param {String} title The string that will show in head of this component page.
+ */
+class RouteComponent {
+  constructor(component, groupPath, endpoint) {
+    this.component = component;
+    this.path = `${groupPath}${endpoint}`;
+    this.group = groupPath;
+  }
 }
 
-
+const components = {
+  login: new RouteComponent(Login, "/login", ""),
+};
 
 const allowPages = {
-    guest:{
-        routes : [components.login],
-        redirect: components.login.path
-    },
-    user: {
-        routes : [],
-        redirect:'/'
-    }
-}
+  guest: {
+    routes: [components.login],
+    redirect: components.login.path,
+  },
+  user: {
+    routes: [],
+    redirect: "/",
+  },
+};
 
-
-export default allowPages
+export default allowPages;
